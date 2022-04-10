ae8091b4b280e203af6f291062a8da0ab677c5b3
Akshat-C <85225437+Akshat-C@users.noreply.github.com>
2022-04-11 09:52:51 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,26 @@ function preload()
 
 function setup()
 {
-    canvas = createCanvas(530, 470);
+    canvas = createCanvas(550, 500);
     canvas.position(900, 220);
+    video = createCapture(VIDEO);
+    video.position(50, 230);
+    pose_model = ml5.poseNet(video, modelLoaded);
+    pose_model.on("pose", gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log("PoseNet has been loaded succesfully");
+}
+
+function draw()
+{}
+
+function gotPoses(results)
+{
+    if (results.length > 0)
+    {
+        console.log(results);
+    }
 }
