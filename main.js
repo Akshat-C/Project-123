48d7372bba6fe03d6ca24328713c315d682d9ea1
Akshat-C <85225437+Akshat-C@users.noreply.github.com>
2022-04-15 09:35:34 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX = 0;
+noseY = 0;
+lwristX = 0;
+rwristX = 0;
+difference = 0;
+
 function preload()
 {}
 
@@ -17,12 +23,26 @@ function modelLoaded()
 }
 
 function draw()
-{}
+{
+    background("blue");
+    textSize(difference);
+    stroke("green")
+    fill("orange");
+    text("Akshat", noseX, noseY);
+}
 
 function gotPoses(results)
 {
     if (results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("Nose x + y values: x- "+noseX+" y- "+noseY);
+        lwristX = results[0].pose.leftWrist.x;
+        rwristX = results[0].pose.rightWrist.x;
+        console.log("Wrist values: leftwrist- "+lwristX+" rightwrist- "+rwristX);
+        difference = floor(lwristX - rwristX);
+        console.log(difference);
     }
 }
